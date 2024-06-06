628d86dd97d287c03c04c149a1619abad69ab18e
TalhaRafique1 <162029206+TalhaRafique1@users.noreply.github.com>
2024-06-06 16:45:07 -0700

    45 Question assigment complete

---
+++ Q-43/app.ts
@@ -0,0 +1,31 @@
+// Define the function to show magicians names
+function show_magicians(magicians: string[]): void{
+    magicians.forEach(name => console.log(name))
+}
+
+// Function to make magicians great through .map() it will modify array
+function make_great(magicians: string[]){
+    return magicians.map(name => `The Great ${name}`);
+}
+
+//  Define an array of magicians names
+let magician_names = ["Harry Poter", "Faizan", "Haseeb"]
+
+// Making a copy of orignal array through .Slice() function
+
+let copy_magician_names = magician_names.slice()
+
+// Modify the copied array to include "The Great" with their names
+let copy_great_magician = make_great(copy_magician_names);
+
+// Show both  arrays Original and copied
+
+// Original
+console.log("Original Array\n");
+
+show_magicians(magician_names)
+
+// Copied
+console.log("\nCopied Array\n");
+
+show_magicians(copy_great_magician)
